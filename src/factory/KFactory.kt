7da6a962635db8bb2e946e689e149922f43f212a
csgsky <674143140@qq.com>
2020-05-12 15:28:18 +0800

    工厂方法和抽象工厂

--- src/factory/KFactory.kt
+++ src/factory/KFactory.kt
@@ -7,6 +7,10 @@ interface Shape {
 }
 
 class Circle : Shape {
+    constructor() {
+        println("Circle")
+    }
+
     override fun draw() {
         println("draw circle")
     }
@@ -31,5 +35,96 @@ class KSimpleFactory {
 }
 
 // 工厂方法模式，又称多态性工厂模式
+interface KFactory {
+    fun getShape(): Shape
+}
+
+class KCircleFactory : KFactory {
+    override fun getShape(): Shape {
+        return Circle()
+    }
+}
+
+class KRectangleFactory : KFactory {
+    override fun getShape(): Shape {
+        return Rectangle()
+    }
+}
 
 // 抽象工厂模式
+/**
+ * 生产枪和对应的子弹，枪：AK，M4A1
+ */
+interface Gun {
+    fun shooting()
+}
+
+interface Bullet {
+    fun load()
+}
+
+class AK: Gun {
+    override fun shooting() {
+        println("shooting with AK")
+    }
+}
+
+class M4A1 : Gun {
+    override fun shooting() {
+        println("shooting with M4A1")
+    }
+}
+
+class AK_Bullet: Bullet {
+    override fun load() {
+        println("load bullet with AK")
+    }
+}
+
+class M4A1_Bullet: Bullet {
+    override fun load() {
+        println("load bullet with M4A1")
+    }
+}
+
+interface KAbstractFactory {
+    fun produceGun(): Gun
+    fun produceBullet(): Bullet
+}
+
+class AK_Factory: KAbstractFactory {
+    override fun produceGun(): Gun {
+        return AK()
+    }
+
+    override fun produceBullet(): Bullet {
+        return AK_Bullet()
+    }
+}
+
+class M4A1Factory: KAbstractFactory {
+    override fun produceGun(): Gun {
+        return M4A1()
+    }
+
+    override fun produceBullet(): Bullet {
+        return M4A1_Bullet()
+    }
+}
+
+
+
+
+
+fun main() {
+    // 简单工厂
+//    KSimpleFactory.getShape("Circle")?.draw()
+    // 工厂方法
+//    val circleFactory = KCircleFactory()
+//    circleFactory.getShape().draw()
+    // 抽象工厂
+    AK_Factory().produceGun().shooting()
+    AK_Factory().produceBullet().load()
+
+
+}
